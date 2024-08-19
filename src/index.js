466c2acb5167d3754b37e2a1acfddc34996b46cf
Luis Otavio <luismatheus052@gmail.com>
2024-08-19 09:50:20 -0300

    Adicionando tratamento de erros

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 
 const fs = require('fs');//faz a importação da bibliotaca FileSistem
+const trataErros = require('./erros/funcoesErros');
+
 
 const caminhoArquivo = process.argv;// modulo que permite que o node receba parametros em um array no teminal
 const link = caminhoArquivo[2];// recebe o parametro passado no teminal no indice 2
 
-fs.readFile(link, 'utf-8', (erro, texto) => {//inicia a função readFile, nativa da biblioteca FS, que faz a leitura do arquivo. Por ser um asquivo de texto é necessario utiliza o parametro UTF-8 que padroniza os caracteres.
-    verificaPalavrasDuplicadas(texto);       //depois ela recebe uma função de seta, com erro e com texto para ser lido
+fs.readFile(link, 'utf-8', (erro, texto) => {//inicia a função readFile, nativa da biblioteca FS, que faz a leitura do arquivo. Por ser um arquivo de texto é necessario utiliza o parametro UTF-8 que padroniza os caracteres.
+    try {
+        if (erro) throw erro
+        contaPalavra(texto);
+    } catch (erro) {
+        trataErros(erro);
+    }
 })
 
+function contaPalavra(texto){
+    const paragrafos = extraiParagrafo(texto);
+    const contagem = paragrafos.flatMap((paragrafo) => {
+        if(!paragrafo)return [];
+        return verificaPalavrasDuplicadas(paragrafo);
+    })
+    
+    console.log(contagem);
+    
+}
+function extraiParagrafo(texto){
+    return texto.toLowerCase().split('\n');// deste modo iremos quebrar a  cada aparição do \n que é um caracter que indica a quebra de linha. Também iremos transformar tudo em minuscula
+}
 
+
+function limpaPalavras(palavra){
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');// o replaca ira trocar um caracter por outro, no caso esta trocadno um caracter especial por um vazio
+}
 
 function verificaPalavrasDuplicadas(texto) {// inicai a função que separa as palavras do texto
     const listaPalavras = texto.split(' '); //utiliza o metodo de array para separar as stings a cada espaço
     const resultado = {};//cria o objeto para gardar o resultado
     listaPalavras.forEach(palavra => { // o forEach que vai percorrer as palavras do array
-        resultado[palavra] = (resultado[palavra] || 0)+1// parra cada incidencia da palavra vai adicionar um ao seu elemento no obj
-    })
-    console.log(resultado)
+        if(palavra.length >=3){
+        const palavraLimpa = limpaPalavras(palavra);
+        resultado[palavraLimpa] = (resultado[palavraLimpa] || 0)+1// parra cada incidencia da palavra vai adicionar um ao seu elemento no obj
+    } })
+    return resultado;
 }
 
